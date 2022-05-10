8188a36197268decbb305f74f61b143eb0935ac4
JitendraVS <jitendrah955@gmail.com>
2022-05-10 18:38:28 +0530

    log added

--- actions/pmd-analyser.sh
+++ actions/pmd-analyser.sh
@@ -30,6 +30,7 @@ else
     pmd-bin-"${PMD_VERSION}"/bin/run.sh pmd -filelist diff-file.csv -R "$RULES_PATH" -failOnViolation false -f sarif > pmd-raw-output.sarif
 fi
 # Loop through each rule and see if an error should be thrown
+echo "kkkkkkkkkkkk"+"$ANALYSE_ALL_CODE"
 echo "::set-output name=error-found::false"
 while read -r rule; do
     RULE="$(echo "$rule" | jq --raw-output '.id')"
